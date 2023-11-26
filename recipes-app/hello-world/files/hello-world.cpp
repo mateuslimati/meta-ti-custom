6bd3df6a47bef77d95e4a6d6b7cfa5c1edefccf8
Mateus Alves <mateuslima.ti@gmail.com>
2023-11-26 13:37:22 -0300

    first commit

---
+++ recipes-app/hello-world/files/hello-world.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file hello-world.cpp
+ * @author Mateus Alves (mateuslima.ti@gmail.com)
+ * @brief A simple C++ hello
+ * @version 0.1
+ * @date 2023-11-25
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "***********************************************" << std::endl;
+    std::cout << "*                                             *" << std::endl;
+    std::cout << "*     Yocto Project Meta TI Custom v1.0       *" << std::endl;
+    std::cout << "*                                             *" << std::endl;
+    std::cout << "***********************************************" << std::endl;
+    return 0;
+}
---
+++ recipes-kernel/linux-bb.org/linux-bb.org_%.bbappend
@@ -0,0 +1,5 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
+
+COMPATIBLE_MACHINE:scorpion = "scorpion"
+
+SRC_URI:append = "file://0001-my-linux-modifications.patch"
